ba09978d78bc0bbe0c79018269364d08580ffeec
Gabriel <agbalbuenaluna@gmail.com>
2022-07-22 02:48:34 -0500

    Adding Basics Listeners to the Views

--- app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
@@ -3,20 +3,37 @@ package com.example.recyclerviewexample.adapter
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recyclerviewexample.R
 import com.example.recyclerviewexample.SuperHero
 import com.example.recyclerviewexample.databinding.ItemSuperheroBinding
 
-class SuperHeroViewHolder(view: View):RecyclerView.ViewHolder(view){
+class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     val binding = ItemSuperheroBinding.bind(view)
 
-    fun render(superHeroModel: SuperHero){
+    fun render(superHeroModel: SuperHero) {
         binding.tvSuperHeroName.text = superHeroModel.superHeroName
         binding.tvRealName.text = superHeroModel.realName
         binding.tvRealName.text = superHeroModel.publisher
         Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
+        //Listener on ImageView
+        binding.ivSuperHero.setOnClickListener {
+            Toast.makeText(
+                binding.ivSuperHero.context,
+                superHeroModel.realName,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        //Listener on List Item
+        itemView.setOnClickListener {
+            Toast.makeText(
+                binding.ivSuperHero.context,
+                superHeroModel.superHeroName,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
